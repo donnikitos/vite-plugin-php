d24711f1b7a879ef75b8910ff9b557074ff76725
Nikita Nitichevski <me@donnikitos.com>
2023-08-27 19:58:40 +0200

    saving tokenized code in files

--- src/utils/escapePHP.ts
+++ src/utils/escapePHP.ts
@@ -1,18 +1,13 @@
-import { readFileSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
 import makeID from './makeID';
-import { resolve } from 'path';
 import writeFile from './writeFile';
 
-export const codeTokens: Map<string, Map<string, string>> = new Map();
+type CodeTokens = Record<string, string>;
 
 export function escapePHP(inputFile: string, outputFile: string) {
-	const fileId = resolve(outputFile);
 	const input = readFileSync(inputFile).toString();
 
-	if (!codeTokens.has(fileId)) {
-		codeTokens.set(fileId, new Map());
-	}
-	const fileTokens = codeTokens.get(fileId)!;
+	const codeTokens: CodeTokens = {};
 
 	const isJS = inputFile.includes('.js') || inputFile.includes('.ts');
 	const isML = inputFile.includes('.php') || inputFile.includes('.htm');
@@ -26,24 +21,26 @@ export function escapePHP(inputFile: string, outputFile: string) {
 			token = `<!--${token}-->`;
 		}
 
-		fileTokens.set(token, match);
+		codeTokens[token] = match;
 
 		return token;
 	});
 
+	writeFile(outputFile + '.json', JSON.stringify(codeTokens));
 	writeFile(outputFile, out);
 }
 
 export function unescapePHP(file: string) {
-	const fileId = resolve(file);
 	const input = readFileSync(file).toString();
-	const fileTokens = codeTokens.get(fileId);
 	let out = input;
 
-	if (fileTokens) {
-		fileTokens.forEach((code, token) => {
+	const tokensFile = file + '.json';
+	if (existsSync(tokensFile)) {
+		const codeTokens = JSON.parse(readFileSync(tokensFile).toString());
+
+		Object.entries(codeTokens).forEach(([token, code]) => {
 			out = out.replace(token, (match) => {
-				return code;
+				return `${code}`;
 			});
 		});
 	}
